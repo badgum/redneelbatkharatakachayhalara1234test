496b32c1b1e29b173fbcc6044403566a726407ed
nsaqib1 <nsaqib32@gmail.com>
2018-02-04 23:43:55 +0600

    Add files via upload

---
+++ index/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+      Redneel
+    </title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="header.css">
+  </head>
+
+  <body>
+    <div id="gh" class="bwh ghh w100"><!--grand header color white-->
+      <div id="ph" class="ghh w100"><!-- parent header -->
+
+        <div id="lasb" class="fl"><!-- logo and search bar -->
+          <div id="logo" class="fl">
+            <a href="index.php"><!-- homepage anchored logo -->
+              <img src="img/final3.png" alt="Logo">
+            </a>
+          </div>
+          <div id="salwr"><!-- sal wrapper -->
+            <div id="sal" class="fr"><!-- search and luck -->
+
+              <form id="sab" class="fl sbh"><!-- search and button -->
+                <input type="text" id="search" class="s fl sbh" placeholder="I am looking for...">
+                <input type="submit" id="button" class="s fl sbh" value="Search">
+              </form>
+            </div><!-- sal -->
+        </div><!-- salwr -->
+        </div><!-- lasb -->
+
+
+
+        <div id="tbwr" class="fr"><!-- three button wrapper but originally its four -->
+          <div id="ulwr" class="fr ghh sau">
+            <ul id="bn-box" class="fr"><!-- button -->
+              <li id="bn">
+                <a href="" class="">
+                  <div class="bn-i"><!-- button image -->
+                    <img src="img/marketing2.png">
+                  </div>
+                  <div class="bn-t fl"><!-- button text -->
+                    M
+                  </div>
+                </a>
+              </li>
+
+              <li id="bn">
+                <a href="" class="">
+                  <div class="bn-i"><!-- button image -->
+                    <img src="img/publisher2.png">
+                  </div>
+                  <div class="bn-t fl"><!-- button text -->
+                    P
+                  </div>
+                </a>
+              </li>
+
+              <li id="bn">
+                <a href="signup/" class="">
+                  <div class="bn-i"><!-- button image -->
+                    <img src="img/sign.png">
+                  </div>
+                  <div class="bn-t fl"><!-- button text -->
+                    Sign Up
+                  </div>
+                </a>
+              </li>
+
+              <li id="bn">
+                <a href="login/" class="">
+                  <div class="bn-i"><!-- button image -->
+                    <img src="img/login.png">
+                  </div>
+                  <div class="bn-t fl"><!-- button text -->
+                    Login
+                  </div>
+                </a>
+              </li>
+
+            </ul>
+          </div><!-- ulwr -->
+        </div><!-- tbwr -->
+      </div><!-- parent header -->
+    </div><!-- grand header -->
+
+    <div id="mosto">
+      <div id="mb"></div><!-- mosto barrier -->
+      <div id="mosto-box">
+        <div id="prdct" class="mocl"></div><!-- mosto class left margin -->
+        <div id="srvcs" class="mocl"></div>
+        <div id="plcs" class="mocl"></div><!-- right margin -->
+      </div>
+    </div>
+
+    <div id="master">
+      <div id="mc"><!-- main content -->
+        <div id="mca" class="fl"><!-- main content area -->
+          <div id="ad-a" class="fl"><!-- advertising area -->
+
+          </div>
+        </div>
+
+          <div id="sc-h" class="fr"><!-- side chick's header -->
+            <div id="sc-bar" class="fr"></div>
+          </div>
+          <div id="sc-b" class="fr"><!-- side chick's body -->
+          </div>
+
+      </div>
+      <div id="footer" class="fl"></div>
+
+    </div>
+  </body>
+</html>
